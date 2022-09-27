ea3547641903a2ec79ffb725286f9ba497a228a9
kinsleehammonds <kinslee.hammonds@gmail.com>
2022-09-27 13:47:59 -0400

    making classes

---
+++ RISC-V Pipline/Instruction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace RISC_V_Pipline
+{
+
+    internal class Instruction
+    {
+        private InstructionType operand { get; set; }
+        private string reg1;
+        private string reg2;
+        private int offset;
+
+        public Instruction(InstructionType _operand, string reg1, string reg2, int offset)
+        {
+            this.operand = _operand;
+            this.reg1 = reg1;
+            this.reg2 = reg2;
+            this.offset = offset;
+        }
+    }
+
+    internal enum InstructionType
+    {
+        MEMORY, ARITHMETIC, EXECUTE
+    }
+
+}
+
---
+++ RISC-V Pipline/Pipeline.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RISC_V_Pipeline
+{
+    public class Pipeline
+    {
+
+        void Fetch()
+        {
+
+        }
+
+        void Decode()
+        {
+
+        }
+
+        void Execute()
+        {
+
+        }
+
+        void Memory()
+        {
+
+        }
+
+        void WriteBack()
+        {
+
+        }
+    }
+}
+
